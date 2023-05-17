1589f6d66ce91ff4c51714d8c773c8db13d7fe1d
saeyeon <saeyeon220@daum.net>
2023-05-17 18:01:04 +0900

    feat: made modal

---
+++ Modal/app.js
@@ -0,0 +1,12 @@
+const openButton = document.getElementById("open");
+const modal = document.querySelector(".modal");
+const overlay = modal.querySelector(".modal_overlay");
+const closeButton = modal.querySelector("button");
+const openModal = () => {
+  modal.classList.remove("hidden");
+};
+const closeModal = () => {
+  modal.classList.add("hidden");
+};
+openButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
