a864cab8e998673c60b13345353ebc846840fe4b
saeyeon <saeyeon220@daum.net>
2023-07-21 17:14:53 +0900

    colorFlipper done

--- Modal/app.js
+++ Modal/app.js
@@ -1,6 +1,5 @@
 const openButton = document.getElementById("open");
 const modal = document.querySelector(".modal");
-const overlay = modal.querySelector(".modal_overlay");
 const closeButton = modal.querySelector("button");
 const openModal = () => {
   modal.classList.remove("hidden");
